04786fceef008b9964c74653e631632703ab3117
Sean <seantully00@hotmail.com>
2016-07-12 00:47:21 +0000

    Mongodb set up

--- server.js
+++ server.js
@@ -2,4 +2,19 @@
 
 var express = require('express');
 var app = express();
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
 var url = process.env.MONGOLAB_URI;
+
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
